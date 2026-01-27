c3a57226dfe05b5201ade27d9cb4866e3e4b5fcd
최진영 <jinyoungchoi@choejin-yeong-ui-MacBookAir.local>
2026-01-27 09:48:38 +0900

    fullstackGPT

---
+++ Home_before.py
@@ -0,0 +1,98 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from datetime import datetime
+
+today = datetime.today().strftime("%H:%M:%S")
+
+st.title(today)
+
+# there is sooooo many function(==widget) using streamlit! Let's see
+# st.title("Hello world!")
+
+# st.subheader("Welcome to Streamlit")
+
+# st.markdown(""" 
+#     #### I love it!
+
+# """)
+
+# st.write("hello")
+# st.write([1,2,3,4,5])
+# st.write({"x":1})
+
+a = [1,2,3,4]
+
+d = {"y":2}
+
+# also, streamlit can show class type like this
+# st.write(PromptTemplate)
+# or just write PromptTemplate
+
+p = PromptTemplate.from_template("xxxx")
+
+# st.write(p)
+# or just write variable
+# a
+# d
+# p
+
+model = st.selectbox("Choose yout model", ("GPT-3", "GPT-4"))
+
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
+
+    # streamlit's data flow : whole page refresh!!!
+    name = st.text_input("What is your name?")
+    name
+
+
+
+value = st.slider("temperature", min_value=0.1, max_value=1.0)
+value
+
+
+import streamlit as st
+
+st.title("title")
+
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("somthing")
+
+tab_one, tab_two, tab_three = st.tabs(["A","B","C"])
+
+with tab_one:
+    st.write('a')
+
+with tab_two:
+    st.write('b')
+
+
+with tab_three:
+    st.write("c")
+
+
+import streamlit as st
+import time
+
+st.title("DocumentGPT")
+
+with st.chat_message("human"):
+    st.write("Hellooooooooo")
+
+with st.chat_message("ai"):
+    st.write("how are you")
+
+st.chat_input("Send a message to the ai")
+
+with st.status("Embedding file...", expanded=True) as status:
+    time.sleep(2)
+    st.write("Getting the file")
+    time.sleep(2)
+    st.write("Embedding the file")
+    time.sleep(2)
+    st.write("Caching the file")
+    status.update(label="Error", state="error")
+
